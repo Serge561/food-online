b3d637c8fb1310610f58841edadc3d2a999b32ff
Sergey Kozlov <serge561958@gmail.com>
2023-10-14 23:17:07 +0200

    customer and vendor registration

--- accounts/views.py
+++ accounts/views.py
@@ -11,6 +11,7 @@ from .models import User, UserProfile
 from django.contrib import messages, auth
 
 # from .utils import detectUser, send_verification_email
+from .utils import detectUser
 from django.contrib.auth.decorators import login_required, user_passes_test
 
 from django.core.exceptions import PermissionDenied
@@ -161,82 +162,86 @@ def registerVendor(request):
 #         return redirect("myAccount")
 
 
-# def login(request):
-#     if request.user.is_authenticated:
-#         messages.warning(request, "You are already logged in!")
-#         return redirect("myAccount")
-#     elif request.method == "POST":
-#         email = request.POST["email"]
-#         password = request.POST["password"]
+def login(request):
+    if request.user.is_authenticated:
+        messages.warning(request, "You are already logged in!")
+        # return redirect("myAccount")
+        return redirect("home")
+    elif request.method == "POST":
+        email = request.POST["email"]
+        password = request.POST["password"]
 
-#         user = auth.authenticate(email=email, password=password)
+        user = auth.authenticate(email=email, password=password)
 
-#         if user is not None:
-#             auth.login(request, user)
-#             messages.success(request, "You are now logged in.")
-#             return redirect("myAccount")
-#         else:
-#             messages.error(request, "Invalid login credentials")
-#             return redirect("login")
-#     return render(request, "accounts/login.html")
-
-
-# def logout(request):
-#     auth.logout(request)
-#     messages.info(request, "You are logged out.")
-#     return redirect("login")
+        if user is not None:
+            auth.login(request, user)
+            messages.success(request, "You are now logged in.")
+            # return redirect("myAccount")
+            return redirect("home")
+        else:
+            messages.error(request, "Invalid login credentials")
+            return redirect("login")
+    return render(request, "accounts/login.html")
 
 
-# @login_required(login_url="login")
-# def myAccount(request):
-#     user = request.user
-#     redirectUrl = detectUser(user)
-#     return redirect(redirectUrl)
+def logout(request):
+    auth.logout(request)
+    messages.info(request, "You are logged out.")
+    return redirect("login")
 
 
-# @login_required(login_url="login")
-# @user_passes_test(check_role_customer)
-# def custDashboard(request):
-#     orders = Order.objects.filter(user=request.user, is_ordered=True)
-#     recent_orders = orders[:5]
-#     context = {
-#         "orders": orders,
-#         "orders_count": orders.count(),
-#         "recent_orders": recent_orders,
-#     }
-#     return render(request, "accounts/custDashboard.html", context)
+@login_required(login_url="login")
+def myAccount(request):
+    user = request.user
+    redirectUrl = detectUser(user)
+    return redirect(redirectUrl)
 
 
-# @login_required(login_url="login")
-# @user_passes_test(check_role_vendor)
-# def vendorDashboard(request):
-#     vendor = Vendor.objects.get(user=request.user)
-#     orders = Order.objects.filter(vendors__in=[vendor.id], is_ordered=True).order_by(
-#         "created_at"
-#     )
-#     recent_orders = orders[:10]
+@login_required(login_url="login")
+@user_passes_test(check_role_customer)
+def custDashboard(request):
+    # orders = Order.objects.filter(user=request.user, is_ordered=True)
+    # recent_orders = orders[:5]
+    context = {
+        # #     "orders": orders,
+        # #     "orders_count": orders.count(),
+        # #     "recent_orders": recent_orders,
+        "orders": "some_order"
+    }
+    return render(request, "accounts/custDashboard.html", context)
 
-#     # current month's revenue
-#     current_month = datetime.datetime.now().month
-#     current_month_orders = orders.filter(
-#         vendors__in=[vendor.id], created_at__month=current_month
-#     )
-#     current_month_revenue = 0
-#     for i in current_month_orders:
-#         current_month_revenue += i.get_total_by_vendor()["grand_total"]
 
-#     # total revenue
-#     total_revenue = 0
-#     for i in orders:
-#         total_revenue += i.get_total_by_vendor()["grand_total"]
-#     context = {
-#         "orders": orders,
-#         "orders_count": orders.count(),
-#         "recent_orders": recent_orders,
-#         "total_revenue": total_revenue,
-#         "current_month_revenue": current_month_revenue,
-#     }
-#     return render(request, "accounts/vendorDashboard.html", context)
+@login_required(login_url="login")
+@user_passes_test(check_role_vendor)
+def vendorDashboard(request):
+    vendor = Vendor.objects.get(user=request.user)
+    # orders = Order.objects.filter(vendors__in=[vendor.id], is_ordered=True).order_by(
+    #     "created_at"
+    # )
+    # recent_orders = orders[:10]
+
+    # current month's revenue
+    # current_month = datetime.datetime.now().month
+    # current_month_orders = orders.filter(
+    #     vendors__in=[vendor.id], created_at__month=current_month
+    # )
+    # current_month_revenue = 0
+    # for i in current_month_orders:
+    #     current_month_revenue += i.get_total_by_vendor()["grand_total"]
+
+    # # total revenue
+    # total_revenue = 0
+    # for i in orders:
+    #     total_revenue += i.get_total_by_vendor()["grand_total"]
+    context = {
+        #     "orders": orders,
+        #     "orders_count": orders.count(),
+        #     "recent_orders": recent_orders,
+        #     "total_revenue": total_revenue,
+        #     "current_month_revenue": current_month_revenue,
+        "orders": "some_orders"
+    }
+    return render(request, "accounts/vendorDashboard.html", context)
 
 
 # def forgot_password(request):
